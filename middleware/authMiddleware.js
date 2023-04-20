4826e64600ff4c5383a7162249bfdccbcef54fd5
Joaquin Cardoso Rios <105686879+joaquincardosorios@users.noreply.github.com>
2023-04-20 17:39:46 -0400

    fature/validate-token

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken"
+import Veterinario from "../models/Veterinario.js";
+
+const checkAuth = async (req,res,next) => {
+    let token;
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
+        try {
+            token = req.headers.authorization.split(' ')[1];
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            
+            req.veterinario = await Veterinario.findById(decoded.id).select("-password -token -confirmado");
+            return next()
+        } catch (error) {
+            const e = new Error('Token no valido')
+            return res.status(403).json({msg: e.message})
+        }
+    } 
+    if(!token){
+        const error = new Error('Token no valido o inexistente')
+        return res.status(403).json({msg: error.message})
+    }
+    
+    next()
+}
+
+export default checkAuth;
--- routes/veterinarioRoutes.js
+++ routes/veterinarioRoutes.js
@@ -1,10 +1,17 @@
 import express from 'express'
 const router = express.Router();
-import {registrar ,perfil, confirmar, autenticar } from '../controllers/veterinarioController.js'
+import {
+    registrar,
+    perfil, 
+    confirmar, 
+    autenticar 
+} from '../controllers/veterinarioController.js'
+import checkAuth from '../middleware/authMiddleware.js';
 
 router.post('/', registrar )
-router.get('/perfil', perfil)
 router.get('/confirmar/:token', confirmar)
 router.post('/login', autenticar)
+
+router.get('/perfil',checkAuth, perfil)
 
 export default router;
