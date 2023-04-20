9e3f4ae4f49103a59b4c7378011c1cfda1f83bd2
Joaquin Cardoso Rios <105686879+joaquincardosorios@users.noreply.github.com>
2023-04-20 14:58:39 -0400

    feature/check-user-exist

--- controllers/veterinarioController.js
+++ controllers/veterinarioController.js
@@ -1,9 +1,27 @@
+import Veterinario from "../models/Veterinario.js"
 
 
+const registrar = async (req,res) => {
+    const { email } = req.body
 
-const registrar =  (req,res) => {
-    console.log(req.body)
-    res.json({msje: 'Registrando usuario'})
+    // Prevenir usuarios duplicados
+    const existeUsuario = await Veterinario.findOne({ email })
+
+    if(existeUsuario){
+        const error = new Error('Usuario ya registrado');
+        return res.status(400).json({msg: error.message})
+    }
+
+    try {
+        //Guardar nuevo veterinario
+        const veterinario = new Veterinario(req.body);
+        const veterinarioGuardado = await veterinario.save();
+
+        res.json(veterinarioGuardado)
+    } catch (error) {
+        console.log(error)
+    }
+
 }
 
 const perfil =  (req,res) => {
