0d8276ce7756c02c9e3a961bf3ac5aa27a01b562
Joaquin Cardoso Rios <105686879+joaquincardosorios@users.noreply.github.com>
2023-04-21 11:49:49 -0400

    feature/update-delete-patients

--- controllers/pacienteController.js
+++ controllers/pacienteController.js
@@ -21,18 +21,60 @@ const obtenerPaciente = async (req,res) => {
     const { id } = req.params;
     const paciente = await Paciente.findById(id);
 
+    if(!paciente){
+        return res.status(404).json({msg: "Paciente no existe"});
+    } 
     if (paciente.veterinario._id.toString() !== req.veterinario._id.toString()){
         return res.json({msg: "Accion no valida"});
     }
     res.json(paciente)
+    
+    
 }
 
 const actualizarPaciente = async (req,res) => {
+    const { id } = req.params;
+    const paciente = await Paciente.findById(id);
+
+    if(!paciente){
+        return res.status(404).json({msg: "Paciente no existe"});
+    } 
+    if (paciente.veterinario._id.toString() !== req.veterinario._id.toString()){
+        return res.json({msg: "Accion no valida"});
+    }
     
+    // Actualizar paciente
+    paciente.nombre = req.body.nombre || paciente.nombre;
+    paciente.propietario = req.body.propietario || paciente.propietario;
+    paciente.email = req.body.email || paciente.email;
+    paciente.fecha = req.body.fecha || paciente.fecha;
+    paciente.sintomas = req.body.sintomas || paciente.sintomas;
+
+    try {
+        const pacienteActualizado = await paciente.save();
+        res.json(pacienteActualizado)
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 const eliminarPaciente = async (req,res) => {
-    
+    const { id } = req.params;
+    const paciente = await Paciente.findById(id);
+
+    if(!paciente){
+        return res.status(404).json({msg: "Paciente no existe"});
+    } 
+    if (paciente.veterinario._id.toString() !== req.veterinario._id.toString()){
+        return res.json({msg: "Accion no valida"});
+    }
+
+    try {
+        await paciente.deleteOne();
+        res.json({msg: "Paciente eliminado"})
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 
