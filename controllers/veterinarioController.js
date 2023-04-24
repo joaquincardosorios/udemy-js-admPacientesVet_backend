643115bc8799defc457c8194fd6ad3f41e4a4052
Joaquin <joaquin.rios@crecic.cl>
2023-04-24 12:31:40 -0400

    feature/setting-mailtrap

--- controllers/veterinarioController.js
+++ controllers/veterinarioController.js
@@ -1,9 +1,10 @@
 import Veterinario from "../models/Veterinario.js"
 import generarJWT from "../helpers/generarJWT.js"
 import generarId from "../helpers/generarId.js"
+import emailRegistro from "../helpers/emailRegistro.js"
 
 const registrar = async (req,res) => {
-    const { email } = req.body
+    const { email, nombre } = req.body
 
     // Prevenir usuarios duplicados
     const existeUsuario = await Veterinario.findOne({ email })
@@ -17,6 +18,13 @@ const registrar = async (req,res) => {
         //Guardar nuevo veterinario
         const veterinario = new Veterinario(req.body);
         const veterinarioGuardado = await veterinario.save();
+
+        // Enviar el email
+        emailRegistro({
+            email,
+            nombre,
+            token: veterinarioGuardado.token
+        })
 
         res.json(veterinarioGuardado)
     } catch (error) {
--- helpers/emailRegistro.js
+++ helpers/emailRegistro.js
@@ -1,14 +1,31 @@
 import nodemailer from 'nodemailer'
 
-const emailRegistro = () => {
-    var transport = nodemailer.createTransport({
-        host: "sandbox.smtp.mailtrap.io",
-        port: 2525,
+const emailRegistro = async (datos) => {
+    const transporter = nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
         auth: {
-          user: "da1fe00b4881cf",
-          pass: "40616f2d539ee1"
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_PASS
         }
     });
+
+    const { email, nombre, token} = datos;
+    
+    // Enviar Email
+
+    const info = await transporter.sendMail({
+        from: "APV - Administrador de Pacientes de Veterinaria",
+        to: email,
+        subject: 'Comprueba tu cuenta en APV',
+        text: 'Comprueba tu cuenta en APV',
+        html: `<p> Hola: ${nombre}, comprueba tu cuenta en APV. </p>
+            <p> Tu cuenta ya esta lista, solo debes comprobarla en el siguiente enlace:
+            <a href="${process.env.FRONTEND_URL}/confirmar/${token}">Comprobar Cuenta</a></p>
+            <p> Si tu no creaste esta cuenta, puedes ignorar este mensaje.</p>
+        `
+    });
+    console.log("Mensaje enviado: %s", info.messageId)
 }
 
 export default emailRegistro
