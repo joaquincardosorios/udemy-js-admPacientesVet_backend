39b43f3746a751f2e58c5136ad77f24f5a4296d0
Joaquin <joaquin.rios@crecic.cl>
2023-04-24 11:08:59 -0400

    setting-cors

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 import express from 'express';
 import dotenv from 'dotenv'
+import cors from 'cors';
 import conectarDB from './config/db.js';
 import veterinarioRoutes from './routes/veterinarioRoutes.js'
 import pacienteRoutes from './routes/pacienteRoutes.js'
+
 
 const app = express();
 app.use(express.json())
 dotenv.config();
 
 conectarDB();
+
+const dominiosPermitidos = ["http://localhost:5173"];
+
+const corsOptions = {
+    origin: function(origin, callback){
+        if(dominiosPermitidos.indexOf(origin) !== -1){
+            // El origin del request esta permitido
+            callback(null, true);
+        } else{
+            callback(new Error('No permitido por CORS'))
+        }
+    }
+}
+
+app.use(cors(corsOptions))
 
 app.use('/api/veterinarios', veterinarioRoutes);
 app.use('/api/pacientes', pacienteRoutes);
